a465a53be6ed93e156815467b8a19191064c2229
Mer Moyano <moyanomariamercedes@gmail.com>
2020-06-12 16:02:45 -0300

    Add routing module

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import store from './store';
+// Updated to HashRouter for compatibility reasons
+import { HashRouter, Route, Link} from 'react-router-dom';
+// import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+import Home from './components/Home';
 
 
 ReactDOM.render((
   <Provider store={store}>
-    <App />
+    <HashRouter>
+      <Route path="/" component={App}>        
+      <Route exact path="/" component={Home}/>
+      {/* <Route path="login" component={Login} /> */}
+      </Route>
+      {/* TODO review if div needed. Seems to have no effect but double check once app is complete */}
+      {/* <div>
+        route elems here...
+      </div> */}
+    </HashRouter>
   </Provider>
 ), document.getElementById('root'));
