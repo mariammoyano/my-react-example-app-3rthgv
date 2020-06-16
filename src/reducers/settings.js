2858bea825997ed0c629073b9ba20d8d98168110
Mer Moyano <moyanomariamercedes@gmail.com>
2020-06-16 18:21:30 -0300

    Settings form

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -1,5 +1,11 @@
 export default (state = {}, action) => {
   switch (action.type) {
+    case 'SETTINGS_SAVED':
+      return {
+        ...state,
+        inProgress: false,
+        errors: action.error ? action.payload.errors : null
+      };
     case 'ASYNC_START':
       return {
         ...state,
