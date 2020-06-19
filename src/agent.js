7529aea4feb787dc0f8b656a6a57f0b5133c7fc0
Mer Moyano <moyanomariamercedes@gmail.com>
2020-06-19 17:47:31 -0300

    Feed and tags

--- src/agent.js
+++ src/agent.js
@@ -25,14 +25,19 @@ const requests = {
     superagent.put(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody)
 };
 
-const limit = (count, p) => `limit=${count}&offset=${p ? p * count : 0}`
+const limit = (count, p) => `limit=${count}&offset=${p ? p * count : 0}`;
+const encode = encodeURIComponent;
 const Articles = {
   all: page =>
     requests.get(`/articles?${limit(10, page)}`),
   byAuthor: (author, page) =>
-    requests.get(`/articles?author=${encodeURIComponent(author)}&limit=5`),
+    requests.get(`/articles?author=${encode(author)}&${limit(10, page)}`),
+  byTag: (tag, page) =>
+    requests.get(`/articles?tag=${encode(tag)}&${limit(10, page)}`),
   favoritedBy: (author, page) =>
-    requests.get(`/articles?favorited=${encodeURIComponent(author)}&${limit(10, page)}`),
+    requests.get(`/articles?favorited=${encode(author)}&${limit(10, page)}`),
+    feed: page =>
+      requests.get(`/articles/feed?${limit(10, page)}`),
   get: slug =>
     requests.get(`/articles/${slug}`)
 };
@@ -66,10 +71,15 @@ const Profile = {
     requests.del(`/profiles/${username}/follow`)
 };
 
+const Tags = {
+  getAll: () => requests.get('/tags')
+};
+
 export default {
   Articles,
   Auth,
   Comments,
   Profile,
+  Tags,
   setToken: _token => { token = _token; }
 };
