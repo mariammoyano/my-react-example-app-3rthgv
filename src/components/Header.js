30d3a203a88855b8c2b57ceca36bf0daca8c1ff1
Mer Moyano <moyanomariamercedes@gmail.com>
2020-06-19 23:45:38 -0300

    Fix links

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,13 +13,13 @@ const LoggedOutView = props => {
         </li>
         
         <li className="nav-item">
-          <Link to="login" className="nav-link">
+          <Link to="/login" className="nav-link">
             Sign in
           </Link>
         </li>
         
         <li className="nav-item">
-          <Link to="register" className="nav-link">
+          <Link to="/register" className="nav-link">
             Sign up
           </Link>
         </li>
@@ -36,19 +36,19 @@ const LoggedInView = props => {
       <ul className="nav navbar-nav pull-xs-right">
 
         <li className="nav-item">
-          <Link to="" className="nav-link">
+          <Link to="/" className="nav-link">
             Home
           </Link>
         </li>
         
         <li className="nav-item">
-          <Link to="editor" className="nav-link">
+          <Link to="/editor" className="nav-link">
             <i className="ion-compose"></i>&nbsp;New Post
           </Link>
         </li>
         
         <li className="nav-item">
-          <Link to="settings" className="nav-link">
+          <Link to="/settings" className="nav-link">
             <i className="ion-gear-a"></i>&nbsp;Settings
           </Link>
         </li>
